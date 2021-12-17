5d76c744635f25f5cd1e23db07d9051717a6ed2a
HGCucumberX <95116330+HGCucumberX@users.noreply.github.com>
2021-12-17 13:09:32 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+
+
+
+var el_up = document.getElementById("UP");
+var el_down = document.getElementById("DOWN");
+
+window.onload=Fun
+function Fun() {
+	var width = window.innerWidth;
+	var height=window.innerHeight;
+	if(width>height){}
+	else{window.location.href = "index_mobile.html";}
+}
